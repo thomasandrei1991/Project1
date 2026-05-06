c992564c251e9a202fa2767ae3ef2650969dff64
Thomas Andrei <thomasandrei1991@gmail.com>
2026-05-06 22:40:03 +0800

    Completed

--- sample7/function.js
+++ sample7/function.js
@@ -1,6 +1,7 @@
 // Variables
 let profileImg = document.querySelector('.profile-img img');
 let nameTag = document.querySelector('.name-tag');
+let about = document.querySelector('.about');
 let changeBtn = document.querySelector('.change-color');
 
 // Functions for hovering profile image
@@ -19,12 +20,16 @@ function toggleMode() {
     if (isDark) {
         document.body.style.backgroundColor = 'rgb(50, 46, 46)';
         nameTag.style.backgroundColor = 'rgb(50, 46, 46)';
+        about.style.backgroundColor = 'rgb(50, 46, 46)';
         nameTag.style.color = 'white';
+        about.style.color = 'white';
         isDark = false;
     } else {
         document.body.style.backgroundColor = 'white';
         nameTag.style.backgroundColor = 'white';
+        about.style.backgroundColor = 'white';
         nameTag.style.color = 'black';
+        about.style.color = 'black';
         isDark = true;
     }
 }
--- sample7/portfolio.php
+++ sample7/portfolio.php
@@ -7,14 +7,23 @@
         <title>My Portfolio</title>
     </head>
     <body>
+        <div class="logo">
+            <img src="nba-logo.png" alt="NBA Logo">
+        </div>
         <div class="profile-container">
             <div class="profile-img" onmouseover="changeProfileImageOver()" onmouseout="changeProfileImageOut()">
                 <img src="paulgeorge.jpg" alt="">
             </div>
             <div class="name-tag">
-                    <h1>Paul George</h1>
+                    <h1>Paul George</h1><br>
                     <h3>Professional Basketball Player</h3>
+
             </div>
+        </div>
+        <div class="about">
+            <h1>About Paul George</h1>
+            <p>Paul George is a 35-year-old American professional basketball player who plays at the Small Forward or Power Forward position for the Philadelphia 76ers in the NBA. He is 6 feet 8 inches (203cm) tall, weighs 220 pounds (99kg), and has a wingspan of 6 feet 11 inches (211cm).</p><br>
+            <p>PG played college basketball for two seasons at California State University, Fresno, commonly known as Fresno State. He joined the Fresno State Bulldogs for the 2008-2009 season and played through the 2009-2010 season before declaring for the NBA Draft. He was selected as the 10th overall pick by the Indiana Pacers in the 2010 NBA Draft.</p>
         </div>
         <button class="change-color">Change Color</button>
     </body>
